64bcaa290862c472919a23b249b339a631a3524e
Jalol <kilichev3133@gmail.com>
2026-07-29 22:04:31 +0500

    Add constructor and gettes

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,4 +1,25 @@
 package com.example.demo.model;
 
 public class Product {
+    private int id;
+    private String name;
+    private int price;
+
+    Product(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
